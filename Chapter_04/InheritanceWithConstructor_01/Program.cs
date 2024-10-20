a9888a26e02827be34635f06f46a9863ac55fd48
SahilMirashi <sahilmirashi1947@gmail.com>
2024-10-20 22:18:09 +0530

    Added the first version of the project "Inheritance with Constructor" in the solution "Chapter_04"

---
+++ Chapter_04/InheritanceWithConstructor_01/Program.cs
@@ -0,0 +1,49 @@
+﻿// Program to demonstrate inheritance with constructor in C# with Visual Studio
+// Programmer: Sahil Mirashi
+
+// Creating the parent/base class "Vehicle"
+public class Vehicle
+{
+    public string Brand;
+
+    // Implementation of constructor in the parent class "Vehicle"
+    public Vehicle(string brand)
+    {
+        Brand = brand;
+    }
+
+    // Implementation of ShowBrand() method in the parent class "Vehicle"
+    public void ShowBrand()
+    {
+        Console.WriteLine($"Car brand: {Brand} (Parent class method implementation)");
+    }
+}
+
+// Creating the child/derived class "Car"
+public class Car : Vehicle
+{
+    // Calling the parent/base class constructor using base keyword
+    public Car(string brand) : base(brand)
+    {
+        // Empty constructor implementation
+    }
+}
+
+namespace InheritanceWithConstructor_01
+{
+
+    public class Program
+    {
+        // Creating the Main() function where the application starts
+        static void Main()
+        {
+            Console.WriteLine("Program to demonstrate inheritance with constructor in C# with Visual Studio\n");
+
+            // Creating the carObject object using the "Car" class
+            Car carObject = new Car("BMW");
+
+            // Calling the ShowBrand() method of the child class carObject implemented in the parent class "Vehicle"
+            carObject.ShowBrand();
+        }
+    }
+}
