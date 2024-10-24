d84e246e3c150bfc039d2ac72b61e9a0e1c4fd6e
SahilMirashi <sahilmirashi1947@gmail.com>
2024-10-24 21:41:13 +0530

    Added 5 new projects related to Nullable Types to the solution "Chapter_02"

---
+++ Chapter_02/GetValueOrDefault_01/Program.cs
@@ -0,0 +1,30 @@
+﻿// Program to demonstrate GetValueOrDefault() Method in C# with Visual Studio
+// Programmer: Sahil Mirashi
+
+namespace GetValueOrDefault_01
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Program to demonstrate GetValueOrDefault() Method in C# with Visual Studio\n");
+
+            // Declaring a nullable decimal variable.
+            decimal? currency = null;
+
+            // Using the 'GetValueOrDefault()' method returns the value of 'currency'
+            // or a default value (0.0f in the case of float) if it is null.
+            decimal defaultValue = currency.GetValueOrDefault();
+
+            Console.WriteLine($"Default Value: {defaultValue}");
+
+            // Assigning a value to 'currency'.
+            currency = 505.05M;
+
+            // Now, 'GetValueOrDefault()' will return the actual value.
+            decimal actualValue = currency.GetValueOrDefault();
+
+            Console.WriteLine($"Actual Value: {actualValue}");  // Output: 505.05M
+        }
+    }
+}
---
+++ Chapter_02/NullCoalescingOperator_01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Program to demonstrate Null Coalescing Operator in C# with Visual Studio
+// Programmer: Sahil Mirashi
+
+namespace NullCoalescingOperator_01
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Program to demonstrate Null Coalescing Operator in C# with Visual Studio\n");
+
+            // Nullable string declared and initialized with null.
+            string? name = null;
+
+            // Using the null-coalescing operator (??) to provide a default value.
+            string result = name ?? "Nick";
+
+            Console.WriteLine($"Result: {result}"); // Output: Nick
+
+        }
+    }
+}
